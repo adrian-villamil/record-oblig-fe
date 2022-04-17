45cf193e59fc8fb6ebbcc9cd5e3741a1fd3e2bb8
adrian-villamil <adrian.villamil@unillanos.edu.co>
2022-04-16 19:52:26 -0500

    Creé un componente que se comportará como una página en sí y tendrá dentro al componente Formulario.js.

---
+++ src/pages/IngresarDatosPage.js
@@ -0,0 +1,11 @@
+import Formulario from "../components/Formulario"
+
+const IngresarDatosPage = () => {
+    return (
+        <div className="ingresar-datos">
+            <Formulario />
+        </div>
+    )
+}
+
+export default IngresarDatosPage
