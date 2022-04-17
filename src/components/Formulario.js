601bdea353a5c754fe9178523e0b0caf515054dc
adrian-villamil <adrian.villamil@unillanos.edu.co>
2022-04-17 16:40:29 -0500

    Le agregué placeholder a los inputs, también el atributo min a los inputs de tipo número para que el usuario no digite números negativos, y que la fecha de pago pueda ingresar fechas desde el día de hoy en adelante.

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -9,7 +9,7 @@ const Formulario = () => {
             <label>Empresa:</label>
           </div>
           <div className='f-columna c2'>
-            <input type='text'/>
+            <input type='text' placeholder='Ingrese el nombre de la empresa'/>
           </div>
         </div>
         <div className="f-fila">
@@ -17,7 +17,7 @@ const Formulario = () => {
             <label>Nit:</label>
           </div>
           <div className='f-columna c2'>
-            <input type='text'/>
+            <input type='text' placeholder='Ingrese el nit de la empresa'/>
           </div>
         </div>
         <div className="f-fila">
@@ -25,7 +25,7 @@ const Formulario = () => {
             <label>Obligación:</label>
           </div>
           <div className='f-columna c2'>
-            <input type='text'/>
+            <input type='text' placeholder='Ingrese el nombre de la obligación'/>
           </div>
         </div>
         <div className="f-fila">
@@ -33,23 +33,23 @@ const Formulario = () => {
             <label>Fecha de pago:</label>
           </div>
           <div className='f-columna c2'>
-            <input type='date'/>
+            <input type='date' min={new Date().toISOString().split('T')[0]}/>
           </div>
         </div>
         <div className="f-fila">
           <div className='f-columna c1'>
-            <label>Valor a pagar:</label>
+            <label>Valor:</label>
           </div>
           <div className='f-columna c2'>
-            <input type='number'/>
+            <input type='number' min='1' placeholder='Ingrese el valor a pagar'/>
           </div>
         </div>
         <div className="f-fila">
           <div className='f-columna c1'>
-            <label>Periodo de pago:</label>
+            <label>Periodicidad:</label>
           </div>
           <div className='f-columna c2'>
-            <input type='number'/>
+            <input type='number' min='1' placeholder='Ingrese el periodo de pago'/>
           </div>
         </div>
       </form>
