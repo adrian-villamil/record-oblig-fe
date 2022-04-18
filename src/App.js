184525f720b8f7aaccb888c01f287b4ca5654f5e
adrian-villamil <adrian.villamil@unillanos.edu.co>
2022-04-17 23:21:08 -0500

    Se insertó las rutas a la aplicación según los enlaces de la barra de navegación de Layout.js.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import Layout from './pages/Layout';
+import PrincipalPage from './pages/PrincipalPage';
 import IngresarDatosPage from './pages/IngresarDatosPage';
+import MostrarDatosPage from './pages/MostrarDatosPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <IngresarDatosPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <Layout /> }>
+            <Route index element={ <PrincipalPage /> } />
+            <Route path='ingresar-datos' element={ <IngresarDatosPage /> } />
+            <Route path='mostrar-datos' element={ <MostrarDatosPage /> } />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
