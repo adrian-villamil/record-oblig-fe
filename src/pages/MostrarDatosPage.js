3aaaf9e8437ebdb5e7170b621a067cd05124a6f0
adrian-villamil <adrian.villamil@unillanos.edu.co>
2022-04-18 22:40:55 -0500

    Le cambié el nombre de MostrarDatosPage.js por PagosPage.js

--- src/pages/MostrarDatosPage.js
+++
@@ -1,9 +0,0 @@
-const MostrarDatosPage = () => {
-  return (
-    <div className="mostrar-datos">
-      <h1>Mostrar Datos</h1>
-    </div>
-  )
-}
-
-export default MostrarDatosPage
---
+++ src/pages/PagosPage.js
@@ -0,0 +1,9 @@
+const PagosPage = () => {
+  return (
+    <div className="pagos-page">
+      <h1>Pagos</h1>
+    </div>
+  )
+}
+
+export default PagosPage
