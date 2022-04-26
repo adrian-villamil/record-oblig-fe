332a5e10f021eae414e2d4db09f0edcc5d02f851
adrian-villamil <adrian.villamil@unillanos.edu.co>
2022-04-26 09:21:00 -0500

    Se le agregó el hook useEffect para que haga el fetch de los datos con la librería axios, luego se le asigna los datos capturados al componente Empresas y se renderiza al final el componente del mismo.

--- src/pages/EmpresasPage.js
+++ src/pages/EmpresasPage.js
@@ -1,7 +1,22 @@
+import { useState, useEffect } from "react";
+import Empresas from "../components/Empresas";
+import axios from "axios";
+import '../css/EmpresasPage.css'
+
 const EmpresasPage = () => {
+  const [empresas, setEmpresas] = useState([]);
+
+  useEffect(() => {
+    axios("http://localhost:3030/empresas").then(response => {
+      setEmpresas(response.data)
+    }).catch(error => {
+      alert("Hubo un error: " + error)
+    })
+  }, [])
+
   return (
-    <div className="empresas-page">
-      <h1>Empresas</h1>
+    <div className="EmpresasPage">
+      <Empresas empresas={empresas} />
     </div>
   )
 }
