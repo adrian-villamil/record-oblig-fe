f1b145c7174a2fdef5db594c933812307ba13981
adrian-villamil <adrian.villamil@unillanos.edu.co>
2022-05-03 17:52:31 -0500

    Le cambié el nombre a la página RegistrarDatosPage por RegistrarEmpresaPage.

---
+++ src/pages/RegistrarEmpresaPage.js
@@ -0,0 +1,11 @@
+import FormularioEmpresa from "../components/FormularioEmpresa"
+
+const RegistrarEmpresaPage = () => {
+  return (
+    <div className="RegistrarEmpresaPage">
+      <FormularioEmpresa />
+    </div>
+  )
+}
+
+export default RegistrarEmpresaPage
