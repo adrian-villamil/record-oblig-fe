b2ddeabcd110478f5996bb17daaec320ae6e1910
adrian-villamil <adrian.villamil@unillanos.edu.co>
2022-05-03 14:35:39 -0500

    Se eliminó el estado inputs del componente FormularioEmpresa y creé dos estados llamados nombre y nit, el cual contendrán los valores de los dos inputs tags del form. Se elimintó el objeto data y se asignó un objeto diréctamente con los valores de los estados nombre y nit a éste dentro del JSON.stringify().

--- src/components/FormularioEmpresa.js
+++ src/components/FormularioEmpresa.js
@@ -3,24 +3,18 @@ import '../css/FormularioEmpresa.css'
 
 const FormularioEmpresa = () => {
 
-  const [inputs, setInputs] = useState({});
-
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs(values => ({...values, [name]: value}))
-  }
+  const [nombre, setNombre] = useState('');
+  const [nit, setNit] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const data = {
-      nombre_empresa: inputs.nombre_empresa,
-      nit_empresa: inputs.nit_empresa
-    }
     fetch("http://127.0.0.1:8000/api/empresas/create/", {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(data)
+      body: JSON.stringify({
+        nombre_empresa: nombre,
+        nit_empresa: nit
+      })
     }).catch(error => {
       alert("Hubo un error " + error);
     });
@@ -34,7 +28,7 @@ const FormularioEmpresa = () => {
             <label>Nombre de la empresa:</label>
           </div>
           <div className='f-columna c2'>
-            <input type='text' name='nombre_empresa' onChange={handleChange} placeholder='Ingrese el nombre de la empresa' required />
+            <input type='text' name='nombre_empresa' onChange={(e) => {setNombre(e.target.value)}} placeholder='Ingrese el nombre de la empresa' required />
           </div>
         </div>
         <div className="f-fila">
@@ -42,7 +36,7 @@ const FormularioEmpresa = () => {
             <label>Nit de la empresa:</label>
           </div>
           <div className='f-columna c2'>
-            <input type='number' name='nit_empresa' min='1' onChange={handleChange} placeholder='Ingrese el nit de la empresa' required />
+            <input type='number' name='nit_empresa' min='1' onChange={(e) => {setNit(e.target.value)}} placeholder='Ingrese el nit de la empresa' required />
           </div>
         </div>
         <div className='f-fila'>
