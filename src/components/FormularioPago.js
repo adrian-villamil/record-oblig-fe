89192015c15bf063f33c6994e0717d2e0a605da5
adrian-villamil <adrian.villamil@unillanos.edu.co>
2022-05-04 17:07:08 -0500

    Se creó el input valor y se agregó el atributo required en la etiqueta select.

--- src/components/FormularioPago.js
+++ src/components/FormularioPago.js
@@ -8,6 +8,7 @@ const FormularioPago = () => {
   const [obligaciones, setObligaciones] = useState([])
   const [idObligacion, setIdObligacion] = useState('');
   const [fechaPago, setFechaPago] = useState('');
+  const [costo, setCosto] = useState(0);
 
   useEffect(() => {
     axios("http://127.0.0.1:8000/api/obligaciones/").then(response => {
@@ -24,7 +25,8 @@ const FormularioPago = () => {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         id_obligacion: idObligacion,
-        fecha_pago: fechaPago
+        fecha_pago: fechaPago,
+        valor: costo
       })
     }).catch(error => {
       alert("Hubo un error " + error);
@@ -35,12 +37,15 @@ const FormularioPago = () => {
     <div className="FormularioPago">
       <form onSubmit={handleSubmit}>
         <FilaBoxForm esSubmit={false} labelTexto='Nombre de la obligación:'>
-          <select name="id" onChange={(e) => setIdObligacion(e.target.value)}>
+          <select name="id" onChange={(e) => setIdObligacion(e.target.value)} required>
             {obligaciones.map(obligacion => <option key={obligacion.id} value={obligacion.id}>{obligacion.nombre_obligacion}</option>)}
           </select>
         </FilaBoxForm>
         <FilaBoxForm esSubmit={false} labelTexto='Fecha de pago:'>
           <input type='date' name="fecha_pago" min={new Date().toISOString().split('T')[0]} onChange={(e) => setFechaPago(e.target.value)} required />
+        </FilaBoxForm>
+        <FilaBoxForm esSubmit={false} labelTexto='Valor de la obligación:'>
+          <input type='number' name="valor" min='1'placeholder="Ingrese el valor a pagar" onChange={(e) => setCosto(e.target.value)} required />
         </FilaBoxForm>
         <FilaBoxForm esSubmit={true} />
       </form>
