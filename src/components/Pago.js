a63ee549206d432def27cd906b59e4d74386019f
adrian-villamil <adrian.villamil@unillanos.edu.co>
2022-05-06 14:33:16 -0500

    Se modificó el link y se agregó un state.

--- src/components/Pago.js
+++ src/components/Pago.js
@@ -8,7 +8,7 @@ const Pago = ({ pago: { id, id_obligacion, fecha_pago, valor } }) => {
       <td>{id_obligacion}</td>
       <td>{fecha_pago}</td>
       <td>{valor}</td>
-      <td><Link to={`/pagos/editar/${id}`} className='linkEditar'>Editar</Link></td>
+      <td><Link to={`/editar-pago/${id}`} className='linkEditar' state={{id: id, idObligacion: id_obligacion, fechaPago: fecha_pago, valor: valor}}>Editar</Link></td>
     </tr>
   )
 }
